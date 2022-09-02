3a616b5492f409d2bc31158b6003e8dc15bd6ab9
Bastien WEBER <weber.bastien@gmail.com>
2022-09-02 14:58:55 +0200

    :lipstick: Install Styled Components and update App style

---
+++ src/App.styles.ts
@@ -0,0 +1,10 @@
+import styled from "@emotion/styled";
+
+export const StyledApp = styled.div`
+    width: 100vw;
+    height: 100vh;
+    background-color: black;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { StyledApp } from './App.styles';
+import { CarouselItem } from './components/CarouselItem/CarouselItem';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <StyledApp>
+      <CarouselItem 
+        imageUrl='https://picsum.photos/200/300'
+        name='Carousel'
+      />
+    </StyledApp>
   );
 }
 
